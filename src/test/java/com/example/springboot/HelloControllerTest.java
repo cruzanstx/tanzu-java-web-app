62e9e290ea3c0caecd2e9d5114806224fa0cc69d
cruzanstx <gershomrogers@gmail.com>
2022-08-24 14:24:17 -0400

    things have changed

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,7 +21,7 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu!", controller.index());
+        assertEquals("G was here!", controller.index());
 
         mockMvc
             .perform(get("/"))
